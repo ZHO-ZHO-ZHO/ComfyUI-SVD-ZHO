3b969302202c63bb9d17a8cf11e959d725a378e6
ZHO-ZHO-ZHO <140084057+ZHO-ZHO-ZHO@users.noreply.github.com>
2024-02-04 18:24:27 +0800

    Create Advanced.py

---
+++ Advanced.py
@@ -0,0 +1,31 @@
+
+class SVD_Steps_MotionStrength_Seed_Zho:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "steps": ("INT", {"default": 30, "min": 25, "max": 40, "step": 1, "display": "slider"}),
+                "motion_strength": ("INT", {"default": 127, "min": 1, "max": 255, "display": "slider"}),
+                "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+            }
+        }
+
+    RETURN_TYPES = ("INT", "INT", "INT",)
+    RETURN_NAMES = ("steps", "motion_strength", "seed",)
+    FUNCTION = "advanced"
+
+    CATEGORY = "Zho模块组/🎞️SVD"
+
+    def advanced(self, steps, motion_strength, seed):
+        return (steps, motion_strength, seed,)
+
+NODE_CLASS_MAPPINGS = {
+    "SVD_Steps_MotionStrength_Seed_Zho": SVD_Steps_MotionStrength_Seed_Zho,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "SVD_Steps_MotionStrength_Seed_Zho": "🎞️SVD Advanced",
+}
